316606faa1ebc9bf22fc0b4ec3928990825054f8
jack0682 <your_github_email@example.com>
2025-08-25 18:20:30 +0900

    20250825 fix

--- build/turtlebot3_autorace/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/rokey1/turtlebot3_ws/install/turtlebot3_autorace_camera/share/turtlebot3_autorace_camera/package.sh"
-. "/home/rokey1/turtlebot3_ws/install/turtlebot3_autorace_detect/share/turtlebot3_autorace_detect/package.sh"
-. "/home/rokey1/turtlebot3_ws/install/turtlebot3_autorace_mission/share/turtlebot3_autorace_mission/package.sh"
